29588310fbaf391e765ff526d691c1b6050f1a82
saif580 <saifulhasan580@gmail.com>
2021-10-29 03:02:25 +0530

    First commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,27 @@
+import './ExpenseItem.css';
+
+function ExpenseItem(props){
+
+    // const expenseDate=new Date(2021,2,28);
+    // const expenseTitle="Car Insurance";
+    // const expenseAmount=3500;
+    const month=props.date.toLocaleString('en-us', {month: 'long'})
+    const day=props.date.toLocaleString('en-us',{day:'2-digit'})
+    const year=props.date.getFullYear()
+    
+    return(
+        <div className="expense-item">
+            <div>
+                <div>{month}</div>
+                <div>{day}</div>
+                <div>{year}</div>
+            </div>
+            <div className="expense-item__description">
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
+            </div>
+        </div>
+    )
+}
+
+export default ExpenseItem;
